58f22c855849948b0c8543099a61eb9b91f877cb
chunyulu2018 <ibm130909@hotmail.com>
2023-07-12 11:58:20 +0900

    新規

---
+++ src/test/java/com/jp/project/supermarket/SupermarketFrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.project.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketFrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
