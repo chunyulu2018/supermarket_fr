ed9b0d15cd5bfea036e1f67cf5e3b8ba9c9e177d
YuSJ <yushujun1997@126.com>
2023-07-13 17:23:57 +0800

    thymeleafへ移行

--- src/main/java/com/jp/supermarket/controller/IndexController.java
+++ src/main/java/com/jp/supermarket/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.jp.supermarket.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -10,7 +12,28 @@ public class IndexController {
     @GetMapping("/index")
     public String index(Model model){
         model.addAttribute("supermarket","超市管理系统");
-
+        model.addAttribute("test", "This is a test");
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("error", "meet error");
+        return "login";
+    }
+
+    @GetMapping("/error")
+    public String error(Model model){
+
+        return "error";
+    }
+
+//    @PostMapping("/login.do")
+//    public String login(@ModelAttribute User user) {
+//        // do something with user
+//        return "success";
+//    }
 }
+
+
+
