cb2be68b86a6a9ad40bb06bab81626fefb44736f
YuSJ <yushujun1997@126.com>
2023-07-13 21:12:10 +0800

    エラー画面修正

--- src/main/java/com/jp/supermarket/controller/IndexController.java
+++ src/main/java/com/jp/supermarket/controller/IndexController.java
@@ -1,39 +1,36 @@
 package com.jp.supermarket.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String index(Model model){
-        model.addAttribute("supermarket","超市管理系统");
+    public String index(Model model) {
+        model.addAttribute("supermarket", "超市管理系统");
         model.addAttribute("test", "This is a test");
         return "index";
     }
 
     @GetMapping("/login")
-    public String login(Model model){
-        model.addAttribute("error", "meet error");
+    public String login(Model model) {
+//        model.addAttribute("error", "meet error");
         return "login";
     }
 
     @GetMapping("/error")
-    public String error(Model model){
+    public String error(Model model) {
 
         return "error";
     }
-
-//    @PostMapping("/login.do")
-//    public String login(@ModelAttribute User user) {
-//        // do something with user
-//        return "success";
-//    }
 }
+
 
 
 
